c45d705aa3bbcbdd553c6264c1d4ade053d857df
benz <benz.github@gmail.com>
2024-12-22 14:59:04 +0100

    Argument parser

--- arg.bat
+++ arg.bat
@@ -2,14 +2,19 @@
 setlocal EnableDelayedExpansion
 chcp 65001 >nul
 
-
 call :_parse_ %*
 
-:: access the variables in main code
+:main
+:: start of main code
+
+
+rem access the variables in main code
 :: echo user: !user!
 :: echo password: !password!
 :: echo action: !action!
+:: echo default: !_args_!
 
+:: end of main code
 :_end_
 endlocal
 goto :eof
@@ -22,18 +27,25 @@ goto :eof
     for %%i in (!args!) do (
         set "arg=%%i"
         call :!arg! 2>nul || (
+            rem if u want match the order of arguments, add the line that have `rem add:`,
+            rem remove `set /a n -=1` and switch !n! with !m!, in the folowing lines:
+            rem add: if not !m! equ !n! set /a m += 1
             if !n! equ 0 (
+                :: set unused arguments to default variable: _args_
                 set "_args_=!_args_!!arg! "
             ) else (
+                :: set the next argument to the variable name
                 call set "next=%%next[!n!]%%"
                 echo !next!=!arg!
                 set "!next!=!arg!"
-                set /a n -= 1
+                set /a n -= 1 & rem remove this line if u want to use the `rem add:` lines
             )
+            rem add: if !m! equ !n! set /a n = 0 & set /a m = 0
+            :: run a label function after the varialbles are set
             if defined run if !n! equ 0 call :!run! & set "run="
         )
     )
-    :: default value are the arguments that have no flag before them
+    :: default value are the arguments that have no flag before them, and never used
     echo default: !_args_!
     if !n! neq 0 echo Unexpected argument: !arg! ... & exit /b 1
     exit /b 0
@@ -42,17 +54,19 @@ goto :eof
     :-bp
     :--best-practice
         set /a n = 2
-        :: default n = 0, set it to for many arguments to be taken as values for the variables
+        :: default n = 0, set it to how many arguments after to be taken as values for the variables
         set "next[2]=variable-1"
         set "next[1]=variable-2"
-        :: the next argument will be placed in the variable name, 
-        :: make sure to make the order of the index in reverse.
-        :: e.g. -bd value-1 value-2 => next[2]=variable-1=value-1, next[1]=variable-2=value-2
+        :: the next arguments will be placed in the variables name, 
+        :: make sure to make the order of the indexs in reverse, to get the right order
+        :: e.g. -bp value-1 value-2 => next[2]=variable-1=value-1, next[1]=variable-2=value-2
+        :: if u want match the order of arguments, checkout the argument handler for the option.
         set "run=_label_" || rem call :_label_
-        :: call a label function now or wait for the defined varialbles to be set
+        :: call a label function now or wait for the defined varialbles to be set with the 'run' flag
         set "best-practice=true"
         :: optional, set the flag as part of the arguments to call some function later
         exit /b 0
+        :: exit the function with success code 0 to continue, else the handler will do some errors
 
 :: Exapmles
     :-u
